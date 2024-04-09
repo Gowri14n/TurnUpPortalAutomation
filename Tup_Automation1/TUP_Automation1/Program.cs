22525f9c36006b39098863de914fde90b0e0b9f5
Gowri14n <lakshmi14.n@gmail.com>
2024-04-09 15:42:03 +0800

    Adding initial code

---
+++ Tup_Automation1/TUP_Automation1/Program.cs
@@ -0,0 +1,21 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+IWebDriver webDriver = new ChromeDriver();
+webDriver.Manage().Window.Maximize();
+webDriver.Navigate().GoToUrl("http://horse.industryconnect.io/Account/Login");
+IWebElement usernameTextbox = webDriver.FindElement(By.Id("UserName"));
+usernameTextbox.SendKeys("hari");
+IWebElement passwordTextbox = webDriver.FindElement(By.Id("Password"));
+passwordTextbox.SendKeys("123123");
+IWebElement loginButton = webDriver.FindElement(By.XPath("//*[@id='loginForm']/form/div[3]/input[1]"));
+loginButton.Click();
+IWebElement LoginLink = webDriver.FindElement(By.XPath("//*[@id='logoutForm']/ul/li/a"));
+if(LoginLink.Text == "Hello hari!")
+{
+    Console.WriteLine("user has logged in successfully");
+}
+else
+{
+    Console.WriteLine("user has not been logged in");
+}
